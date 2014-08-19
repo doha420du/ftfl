c5e4592237b9df1330fc8b94c5fbb2646c2ca5f1
hasan doha <doha.physics.du@gmail.com>
2014-08-19 17:44:27 +0600

    layout using blade

---
+++ app/views/layout/default.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <link type="text/css" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" />
+</head>
+<body>
+@yield('content')
+
+
+<script type="text/javascript" src="//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+</body>
+</html>
